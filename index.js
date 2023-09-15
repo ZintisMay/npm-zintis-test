59518b34449c692bc38ea9bd7906c0bf3db7998e
ZintisMay <zbmayk@gmail.com>
2023-09-15 10:07:16 -0400

    version update

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = function(){console.log("you have run the npm-zintis-test index.js file, congrats!")}
+module.exports = function(){console.log("you have run the npm-zintis-test index.js file 1.0.2, congrats!")}
